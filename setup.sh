d69fdb62eba4836c3f6450cdac739abcb0253ab5
Jeremiahtaylor2017 <37142633+Jeremiahtaylor2017@users.noreply.github.com>
2025-07-08 22:01:36 -0700

    setup oh-my-zsh custom plugins

--- setup.sh
+++ setup.sh
@@ -17,3 +17,23 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
 else
     echo "Unsupported OS: $OSTYPE"
 fi
+
+echo "Installing custom oh-my-zsh plugins"
+
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+# powerlevel10k
+if [ ! -d "ZSH_CUSTOM/plugins/themes/powerlevel10k" ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
+# zsh-autosuggestions
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+# zsh-syntax-highlighting
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+echo "✅ Plugins installed!"
