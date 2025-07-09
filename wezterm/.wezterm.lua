8512e8f86802c1f6687f1e2ef775f65caea08643
Jeremiahtaylor2017 <37142633+Jeremiahtaylor2017@users.noreply.github.com>
2025-07-08 21:00:03 -0700

    added bg

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,13 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+-- Constants
+local M = {}
+
+M.bg_blurred_darker = wezterm.home_dir .. "/dotfiles/wezterm/assets/bg-blurred-darker.png"
+M.bg_blurred = wezterm.home_dir .. "/dotfiles/wezterm/assets/bg-blurred.png"
+M.bg_image = M.bg_blurred_darker
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
@@ -17,7 +24,17 @@ config.colors = {
 }
 
 -- Appearance
--- config.window_decorations = "RESIZE"
--- config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+config.window_background_image = M.bg_image
+
+-- Miscellaneous settings
+config.prefer_egl = true
 
 return config
